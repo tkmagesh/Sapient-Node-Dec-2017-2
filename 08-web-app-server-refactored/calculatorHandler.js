181a3520ad782dbe2d30954626c0dd29d9498d25
Magesh <tkmagesh77@gmail.com>
2017-12-09 12:39:55 +0530

    refactoring data parsing logic in to dataParser.js

--- 08-web-app-server-refactored/calculatorHandler.js
+++ 08-web-app-server-refactored/calculatorHandler.js
@@ -2,8 +2,8 @@ var querystring = require('querystring'),
 	calculator = require('./calculator');
 
 module.exports = function(req, res, next){
-	if (req.urlObj.pathname === '/calculator' && req.method === 'GET'){
-		var data = querystring.parse(req.urlObj.query),
+	if (req.urlObj.pathname === '/calculator'){
+		var data = req.method === 'GET' ? req.query : req.body,
 			n1 = parseInt(data.n1),
 			n2 = parseInt(data.n2),
 			op = data.op;
@@ -12,22 +12,6 @@ module.exports = function(req, res, next){
 		res.write(result.toString());
 		res.end();
 		next();
-	} else if (req.urlObj.pathname === '/calculator' && req.method === 'POST'){
-		var rawData = '';
-		req.on('data', function(chunk){
-			rawData += chunk;
-		});
-		req.on('end', function(){
-			var data = querystring.parse(rawData),
-				n1 = parseInt(data.n1),
-				n2 = parseInt(data.n2),
-				op = data.op;
-
-			var result = calculator[op](n1, n2);
-			res.write(result.toString());
-			res.end();
-			next();
-		});
 	} else {
 		next();
 	}
--- 08-web-app-server-refactored/dataParser.js
+++ 08-web-app-server-refactored/dataParser.js
@@ -1,8 +1,21 @@
-var url = require('url');
+var url = require('url'),
+	querystring = require('querystring');
 
 module.exports = function(req, res, next){
 	var urlObj = url.parse(req.url);
 	urlObj.pathname = urlObj.pathname === '/' ? '/index.html' : urlObj.pathname;
 	req.urlObj = urlObj;
-	next();
+	req.query = querystring.parse(req.urlObj.query);
+	if (req.method !== 'GET'){
+		var rawData = '';
+		req.on('data', function(chunk){
+			rawData += chunk;
+		});
+		req.on('end', function(){
+			req.body = querystring.parse(rawData);
+			next();
+		});
+	} else {
+		next();
+	}
 }
