ef4a3366db6dea17f31a69cf0d4001e764db3446
Magesh <tkmagesh77@gmail.com>
2017-12-09 09:35:09 +0530

    web app server solution

--- 07-web-app-server/server.js
+++ 07-web-app-server/server.js
@@ -36,14 +36,21 @@ var server = http.createServer(function(req, res){
 		res.write(result.toString());
 		res.end();
 	} else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
-		var data = querystring.parse(urlObj.query),
-			n1 = parseInt(data.n1),
-			n2 = parseInt(data.n2),
-			op = data.op;
+		var rawData = '';
+		req.on('data', function(chunk){
+			rawData += chunk;
+		});
+		req.on('end', function(){
+			var data = querystring.parse(rawData),
+				n1 = parseInt(data.n1),
+				n2 = parseInt(data.n2),
+				op = data.op;
 
-		var result = calculator[op](n1, n2);
-		res.write(result.toString());
-		res.end();
+			var result = calculator[op](n1, n2);
+			res.write(result.toString());
+			res.end();
+		})
+		
 	} else {
 		res.statusCode = 404;
 		res.end();
