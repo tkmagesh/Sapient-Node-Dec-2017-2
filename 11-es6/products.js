827013a8dfa66aebf3de8271a8bc17a2b6ea2f3e
Magesh <tkmagesh77@gmail.com>
2017-12-10 09:17:00 +0530

    es6 assignment

---
+++ 11-es6/products.js
@@ -0,0 +1,12 @@
+var products = [
+	{id : 6, name : 'Pen', cost : 50, units : 20, category : 'stationary'},
+	{id : 9, name : 'Ten', cost : 70, units : 70, category : 'stationary'},
+	{id : 3, name : 'Len', cost : 60, units : 60, category : 'grocery'},
+	{id : 5, name : 'Zen', cost : 30, units : 30, category : 'grocery'},
+	{id : 1, name : 'Ken', cost : 20, units : 80, category : 'utencil'},
+];
+
+1. find all the stationary products
+2. find the sum of product value (product value = cost * units) of all products
+3. find the costliest product
+4. apply a discount of 10% to all the grocery products
